42abc9a13571432ce47222ea3eeb4f18217dd476
aprillialee <aprilliapmlee@gmail.com>
2020-11-22 20:27:42 +0000

    added title and subtitle

--- components/homepage/Homepage.js
+++ components/homepage/Homepage.js
@@ -6,6 +6,10 @@ function HomePage(){
   <HomePageStyled>
     <ImageCover/>
     <img src="/images/hallway.jpeg" alt="hallway image"/>
+    <HomeFeature>
+      <HomeTitle>DESIGN CENTRIC CARPENTRY</HomeTitle>
+      <HomeSubtitle>Beautiful craftsmanship for all of your carpentry needs.</HomeSubtitle>
+    </HomeFeature>
   </HomePageStyled>
   </>
   )
@@ -14,6 +18,7 @@ function HomePage(){
 const HomePageStyled = styled.div`
 width: 100%;
 height: 800px;
+display: flex;
 
 img{
   width: 100%;
@@ -35,7 +40,7 @@ width: 100%;
 height: 800px;
 position: absolute;
 background: black;
-opacity: 30%;
+opacity: 25%;
 
 @media (max-width: 1000px){
   height: 600px;
@@ -43,6 +48,41 @@ opacity: 30%;
 
 @media (max-width: 600px){
   height: 400px;
+}
+`
+
+const HomeFeature = styled.div`
+width: 42%;
+height: 300px;
+margin-left: 10%;
+margin-top: 15%;
+position: absolute;
+@media (max-width: 1000px) {
+  margin-top: 150px;
+  width: 45%;
+}
+`
+
+const HomeTitle = styled.h1`
+font-size: 4.8vw;
+color: white;
+letter-spacing: 3px;
+font-weight: 500;
+`
+
+const HomeSubtitle = styled.h2`
+font-weight: 300;
+letter-spacing: 0.5vw;
+color: white;
+margin-top: -35px;
+font-size: 1.5vw;
+
+@media (max-width: 1000px) {
+  margin-top: -20px;
+}
+
+@media (max-width: 1000px) {
+  margin-top: -10px;
 }
 `
 
